4ef638a77319816f2faebd80e7719e45ae756009
suap-dev <suap.dev@gmail.com>
2022-12-24 23:26:04 +0100

    Create window.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 use bevy::prelude::*;
 
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::WorldInspectorPlugin;
+
 fn main() {
     let mut app = App::new();
 
+    app.insert_resource(WindowDescriptor {
+        title: "Mine Sweeper".to_string(),
+        width: 700.,
+        height: 800.,
+        ..Default::default()
+    })
+    .add_plugins(DefaultPlugins);
+
+    #[cfg(feature="debug")]
+    app.add_plugin(WorldInspectorPlugin::new());
+
+    app.add_startup_system(camera_setup);
+
     app.run();
+}
+
+
+fn camera_setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
